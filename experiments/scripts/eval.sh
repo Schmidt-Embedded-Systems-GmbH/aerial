3b3c67a4896ff5327b1864d4ae748f3f7727282e
traytel <none@none>
2016-10-19 19:25:23 +0200

    tuned experiments

--- experiments/scripts/eval.sh
+++ experiments/scripts/eval.sh
@@ -4,15 +4,15 @@ MONPOLY="./monpoly"
 AERIAL="../../aerial"
 TIME="/usr/bin/time -l"
 
-f1='Until (lclosed_rclosed_BI 0 10, Bool true, P "P1")'
-f2='Until (lclosed_rclosed_BI 0 10, P "P0", P "P1")'
-f3='Until (lclosed_rclosed_BI 0 10, P "P0", Since (B (lclosed_rclosed_BI 2 6), P "P1", P "P2"))'
-f4='Until (lclosed_rclosed_BI 0 10, P "P0", Until (lclosed_rclosed_BI 2 6, P "P1", P "P2"))'
+f1='Until (lclosed_rclosed_BI 0 5, Bool true, P "P1")'
+f2='Until (lclosed_rclosed_BI 0 5, P "P0", P "P1")'
+f3='Until (lclosed_rclosed_BI 0 5, P "P0", Since (B (lclosed_rclosed_BI 2 6), P "P1", P "P2"))'
+f4='Until (lclosed_rclosed_BI 0 5, P "P0", Until (lclosed_rclosed_BI 2 6, P "P1", P "P2"))'
 
-mf1='TRUE UNTIL [0, 10] P1 ()'
-mf2='P0 () UNTIL [0, 10] P1 ()'
-mf3='P0 () UNTIL [0, 10] (P1 () SINCE [2, 6] P2 ())'
-mf4='P0 () UNTIL [0, 10] (P1 () UNTIL [2, 6] P2 ())'
+mf1='TRUE UNTIL [0, 5] P1 ()'
+mf2='P0 () UNTIL [0, 5] P1 ()'
+mf3='P0 () UNTIL [0, 5] (P1 () SINCE [2, 6] P2 ())'
+mf4='P0 () UNTIL [0, 5] (P1 () UNTIL [2, 6] P2 ())'
 
 mkdir -p formulas
 echo $f1 > formulas/f1.formula
@@ -40,24 +40,29 @@ for rate in `cat rates | head -n4`
 do
   for form in {1..4};
   do
-    for mode in {0..2};
+    for mode in {1..2};
     do
       for i in {1..3};
       do
-  		  ($TIME $AERIAL -mode $mode -fmla formulas/f$form.formula -log logs/tr${form}_${i}_${rate}.log -out /dev/null) 2>&1 >/dev/null | grep "maximum resident set size" | sed "s/[ a-z]*//g" > res;
+        print_mode $mode;
+        echo -n " rate: $rate formula: $form idx: $i space: ";
+  		  ($TIME $AERIAL -mode $mode -fmla formulas/f$form.formula -log logs/tr${form}_${i}_${rate}.log -out /dev/null) 2>&1 | grep "maximum resident set size" | sed "s/[ a-z]*//g";
+# > res;
       done;
-      print_mode $mode;
-      echo -n " rate: $rate formula: $form ";
-      perl -lane '$a+=$_ for(@F);$f+=scalar(@F);END{print "space: ".$a/($f * 1024 * 1024)." MB"}' res
-      rm res
+#      print_mode $mode;
+#      echo -n " rate: $rate formula: $form ";
+#      perl -lane '$a+=$_ for(@F);$f+=scalar(@F);END{print "space: ".$a/($f * 1024 * 1024)." MB"}' res
+#      rm res
     done
 
     for i in {1..3};
     do
-      ($TIME $MONPOLY -sig f.sig -formula formulas/monpoly_f$form.formula -log logs/tr${form}_${i}_${rate}.log) 2>&1 >/dev/null | grep "maximum resident set size" | sed "s/[ a-z]*//g" > res
+      echo -n "monpoly rate: $rate formula: $form idx: $i space: ";
+      ($TIME $MONPOLY -sig f.sig -formula formulas/monpoly_f$form.formula -log logs/tr${form}_${i}_${rate}.log) 2>&1 | grep "maximum resident set size" | sed "s/[ a-z]*//g";
+# > res
     done;
-    echo -n "monpoly rate: $rate formula: $form ";
-    perl -lane '$a+=$_ for(@F);$f+=scalar(@F);END{print "space: ".$a/($f * 1024 * 1024)." MB"}' res
-    rm res
+#    echo -n "monpoly rate: $rate formula: $form ";
+#    perl -lane '$a+=$_ for(@F);$f+=scalar(@F);END{print "space: ".$a/($f * 1024 * 1024)." MB"}' res
+#    rm res
   done
 done
