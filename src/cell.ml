ea73d00fcf9f88de4c94cdb7cbeb2c6f0be5db3c
traytel <none@none>
2017-03-29 23:11:38 +0200

    refectored, part 2

---
+++ src/cell.ml
@@ -0,0 +1,130 @@
+(*******************************************************************)
+(*     This is part of Aerial, it is distributed under the         *)
+(*  terms of the GNU Lesser General Public License version 3       *)
+(*           (see file LICENSE for more details)                   *)
+(*                                                                 *)
+(*  Copyright 2017:                                                *)
+(*  Dmitriy Traytel (ETH Zürich)                                   *)
+(*******************************************************************)
+
+open Util
+
+let maybe_output case_cell fmt skip d cell f =
+  case_cell (fun b -> (if skip then fun _ -> () else output_verdict fmt) (d, b); fun x -> x) (f (d, cell)) cell
+
+type cell = V of bool * int | B of bool | C of cell * cell | D of cell * cell
+type future_cell = Now of cell | Later of (timestamp -> cell)
+
+let case_cell_bool f g = function
+  | B b -> f b
+  | _ -> g
+
+let maybe_output_cell fmt = maybe_output case_cell_bool fmt
+
+let maybe_output_future fmt d fcell f =
+  match fcell with
+  | Now cell -> maybe_output_cell fmt false d cell (fun _ -> f)
+  | Later _ -> f
+
+let eval_future_cell t = function
+  | Now c -> c
+  | Later f -> f t
+
+let cconj x y = match x, y with
+  | (B c, d) | (d, B c) -> if c then d else B c
+  | _ -> C (x, y)
+
+let cdisj x y = match x, y with
+  | (B c, d) | (d, B c) -> if c then B c else d
+  | _ -> D (x, y)
+
+let rec cneg = function
+  | C (c, d) -> D (cneg c, cneg d)
+  | D (c, d) -> C (cneg c, cneg d)
+  | B b -> B (not b)
+  | V (b, x) -> V (not b, x)
+
+let is_true = function
+  | B true -> true
+  | _ -> false
+
+let is_false = function
+  | B false -> true
+  | _ -> false
+
+let fcconj x y = match x, y with
+  | (Now c, Now d) -> Now (cconj c d)
+  | (Now c, Later f) | (Later f, Now c) ->
+    if is_true c then Later f
+    else if is_false c then Now c
+    else Later (fun t -> cconj c (f t))
+  | (Later f, Later g) -> Later (fun t -> cconj (f t) (g t))
+
+let fcdisj x y = match x, y with
+  | (Now c, Now d) -> Now (cdisj c d)
+  | (Now c, Later f) | (Later f, Now c) ->
+    if is_true c then Now c
+    else if is_false c then Later f
+    else Later (fun t -> cdisj c (f t))
+  | (Later f, Later g) -> Later (fun t -> cdisj (f t) (g t))
+
+let fcneg = function
+  | Now c -> Now (cneg c)
+  | Later f -> Later (fun t -> cneg (f t))
+
+let maybe_flip b = if b then fun x -> x else cneg
+let maybe_flip_future b = if b then fun x -> x else fcneg
+
+let rec map_cell f = function
+  | C (c, d) -> cconj (map_cell f c) (map_cell f d)
+  | D (c, d) -> cdisj (map_cell f c) (map_cell f d)
+  | B b -> B b
+  | V (b, x) -> maybe_flip b (f x)
+
+let rec map_cell_future f = function
+  | C (c, d) -> fcconj (map_cell_future f c) (map_cell_future f d)
+  | D (c, d) -> fcdisj (map_cell_future f c) (map_cell_future f d)
+  | B b -> Now (B b)
+  | V (b, x) -> maybe_flip_future b (f x)
+
+let subst_cell v = map_cell (Array.get v)
+let subst_cell_future v = map_cell_future (Array.get v)
+
+type bdd = TT | FF | Node of int * bdd * bdd
+
+module IntMap = Map.Make(struct type t = int let compare = compare end)
+
+let rec map_of m k = try Some (IntMap.find k m) with Not_found -> None
+
+let node x t1 t2 = if t1 = t2 then t1 else Node (x, t1, t2)
+
+let rec reduce xs = function
+  | TT -> TT
+  | FF -> FF
+  | Node (x, t1, t2) -> match map_of xs x with
+    | None -> node x (reduce (IntMap.add x true xs) t1) (reduce (IntMap.add x false xs) t2)
+    | Some b -> reduce xs (if b then t1 else t2)
+
+let rec norm xs x t1 t2 = match x with
+  | TT -> reduce xs t1
+  | FF -> reduce xs t2
+  | Node (x, u1, u2) -> match map_of xs x with
+    | None -> node x (norm (IntMap.add x true xs) u1 t1 t2) (norm (IntMap.add x false xs) u2 t1 t2)
+    | Some true -> norm xs u1 t1 t2
+    | Some false -> norm xs u2 t1 t2
+
+let rec bdd_of = function
+  | B b -> if b then TT else FF
+  | V (true, i) -> Node (i, TT, FF)
+  | V (false, i) -> Node (i, FF, TT)
+  | C (b1, b2) -> norm IntMap.empty (bdd_of b1) (bdd_of b2) FF
+  | D (b1, b2) -> norm IntMap.empty (bdd_of b1) TT (bdd_of b2)
+
+let rec size = function
+  | C (b1, b2) | D (b1, b2) -> 1 + size b1 + size b2
+  | _ -> 0
+
+let rec equiv b1 b2 =
+  let t1 = bdd_of b1 in
+  let t2 = bdd_of b2 in
+  TT = norm IntMap.empty t1 t2 (norm IntMap.empty t2 FF TT)
---
+++ src/cell.mli
@@ -0,0 +1,31 @@
+(*******************************************************************)
+(*     This is part of Aerial, it is distributed under the         *)
+(*  terms of the GNU Lesser General Public License version 3       *)
+(*           (see file LICENSE for more details)                   *)
+(*                                                                 *)
+(*  Copyright 2017:                                                *)
+(*  Dmitriy Traytel (ETH Zürich)                                   *)
+(*******************************************************************)
+
+open Util
+
+type cell = V of bool * int | B of bool | C of cell * cell | D of cell * cell
+type future_cell = Now of cell | Later of (timestamp -> cell)
+
+val maybe_output_cell: out_channel -> bool -> timestamp * int -> cell -> ((timestamp * int) * cell -> 'a -> 'a) -> 'a -> 'a
+val maybe_output_future: out_channel -> timestamp * int -> future_cell -> ('a -> 'a) -> 'a -> 'a
+
+val cconj: cell -> cell -> cell
+val cdisj: cell -> cell -> cell
+val cneg: cell -> cell
+
+val fcconj: future_cell -> future_cell -> future_cell
+val fcdisj: future_cell -> future_cell -> future_cell
+val fcneg: future_cell -> future_cell
+
+val eval_future_cell: timestamp -> future_cell -> cell
+
+val subst_cell: cell array -> cell -> cell
+val subst_cell_future: future_cell array -> cell -> future_cell
+
+val equiv: cell -> cell -> bool
